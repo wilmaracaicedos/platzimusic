aebd95437adff3198cc29155b38c3e8cc790c3ba
Wilmar Caicedo <wilmarcaicedo@hotmail.com>
2019-01-12 13:01:04 -0500

    actualizando datos en la interfaz

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 #app
   img(src='./assets/logo.png')
   h1 PlatziMusic
+  select(v-model="selectedCountry")
+    option(v-for="country in countries" :value="country.value") {{ country.name }}
   ul
     artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 
@@ -15,7 +17,13 @@ export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Colombia', value: 'colombia' },
+        { name: 'España', value: 'spain' },
+      ],
+      selectedCountry: 'argentina'
     }
   },
 
@@ -23,15 +31,24 @@ export default {
     Artist
   },
 
-  mounted: function () {
-    const self = this
-    getArtists()
-      .then(function (artists) {
-        self.artists = artists
-      })
-      /*.then(artists => {
-        this.artists = artists
-      })*/
+  methods: {
+    refreshArtists() {
+      const self = this
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.artists = artists
+        })
+    }
+  },
+
+  mounted() {
+    this.refreshArtists()
+  },
+
+  watch: {
+    selectedCountry() {
+      this.refreshArtists()
+    }
   }
 }
 </script>
