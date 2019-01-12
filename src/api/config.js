a866f376a3845194062f611a8774c32b9aa0ecb2
Wilmar Caicedo <wilmarcaicedo@hotmail.com>
2019-01-12 10:23:43 -0500

    configurando api Lastfm

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: 'a63adff172db08f1ee0394c2cdf569cf',
+    secret: '3419da37734eb3fa16b9f0ce3cbd0560',
+    registeredTo: 'wilmaracaicedos'
+}
